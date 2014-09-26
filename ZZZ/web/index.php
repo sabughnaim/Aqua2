8f4874db208627656c5501c260b89ffab5e7b348
Yaliang <yaliangwang2015@u.northwestern.edu>
2014-09-26 11:49:45 -0600

    play audio
    add playAudio(source, time) on functions.js

--- ZZZ/web/index.php
+++ ZZZ/web/index.php
@@ -5,20 +5,22 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>ZZZ-Naps made Easy</title>
-   <link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
-   <link href="./home.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
+    <script src="./Scripts/jquery-1.9.1.min.js"></script>
+    <script src="./Scripts/bootstrap.min.js"></script>
+    <script src="./Scripts/functions.js"></script>
+    <link href="./home.css" rel="stylesheet">
     <link href="./Content/bootstrap.min.css" rel="stylesheet">
     </head > 
     <body>
         <div class="container-fluid" style="text-align: center">
         <h1 style=" color: white;
-    text-shadow: 2px 2px 1px lightgray;
-    font-size: 80px">ZZZ</h1> 
+    		    text-shadow: 2px 2px 1px lightgray;
+    		    font-size: 80px">ZZZ</h1> 
         <a href="#" class="btn btn-sample btn-lg">Nap Time</a>
         </div>
-    <script src="./Scripts/jquery-1.9.1.min.js">
-    </script>
-    <script src="./Scripts/bootstrap.min.js">
-    </script>
+	<script>
+	    playAudio("http://192.168.1.12/upload/ts_music.mp3",10000);
+	</script>
     </body>
 </html>
